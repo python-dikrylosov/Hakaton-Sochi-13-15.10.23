afc40e77939f1ba13e7215606abe07cff3339d2b
python_dikrylosov <48491372+python-dikrylosov@users.noreply.github.com>
2023-10-14 03:58:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ param_size = pars_smp_train.shape[1]
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(200, 3)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(256, activation='relu'),  
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(512, activation='relu'), 
+    tf.keras.layers.Dense(1024, activation='relu'), 
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(128, activation='relu'),
